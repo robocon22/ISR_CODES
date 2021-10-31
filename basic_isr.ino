97223de75d828b12d0c75fb77fd2c10b0be98077
robocon22 <84019266+robocon22@users.noreply.github.com>
2021-10-31 00:29:49 -0700

    Update basic_isr.ino

--- basic_isr.ino
+++ basic_isr.ino
@@ -1,14 +1,16 @@
-volatile boolean led=false;
+volatile boolean led=false;                           //Defining the state of the led as global volatile variable 
 
 void setup() {
   pinMode(13,OUTPUT);
   pinMode(2,INPUT);
-  attachInterrupt(0,led_on_off,RISING);
+  attachInterrupt(0,led_on_off,RISING);              //assigning the interrupt service routine for the interrupt pin2 = 0 at the rising of the pulse
 }
 
 void loop() {
   
 }
+
+// changing the state of led according to the interrupt
 
 void led_on_off()
 {
